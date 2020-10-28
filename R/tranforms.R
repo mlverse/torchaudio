9dfa3c83162b6fe8a61899eb382cfacb876b5e62
Athos Damiani <athos.damiani@gmail.com>
2020-10-27 23:47:16 -0300

    transform_mfcc() complete

--- R/tranforms.R
+++ R/tranforms.R
@@ -65,9 +65,9 @@ transform_spectrogram <- torch::nn_module(
 #' @param n_mels (int, optional): Number of mel filterbanks. (Default: ``128``)
 #' @param sample_rate (int, optional): Sample rate of audio signal. (Default: ``16000``)
 #' @param f_min (float, optional): Minimum frequency. (Default: ``0.``)
-#' @param f_max (float or None, optional): Maximum frequency. (Default: ``sample_rate // 2``)
+#' @param f_max (float or NULL, optional): Maximum frequency. (Default: ``sample_rate // 2``)
 #' @param n_stft (int, optional): Number of bins in STFT. Calculated from first input
-#' if None is given.  See ``n_fft`` in :class:`Spectrogram`. (Default: ``None``)
+#' if NULL is given.  See ``n_fft`` in :class:`Spectrogram`. (Default: ``NULL``)
 #'
 #' @return `tensor`: Mel frequency spectrogram of size (..., ``n_mels``, time).
 #'
@@ -91,7 +91,7 @@ transform_mel_scale <- torch::nn_module(
     fb = if(is.null(n_stft)) {
       torch::torch_empty(0)
     } else {
-      create_fb_matrix(
+      functional_create_fb_matrix(
         n_freqs = n_stft,
         f_min = f_min,
         f_max = f_max,
@@ -175,6 +175,89 @@ transform_amplitude_to_db <- torch::nn_module(
   }
 )
 
+#' Mel Spectrogram
+#'
+#' Create MelSpectrogram for a raw audio signal. This is a composition of Spectrogram
+#'   and MelScale.
+#'
+#' @param waveform (Tensor): Tensor of audio of dimension (..., time).
+#' @param sample_rate (int, optional): Sample rate of audio signal. (Default: ``16000``)
+#' @param win_length (int or NULL, optional): Window size. (Default: ``n_fft``)
+#' @param hop_length (int or NULL, optional): Length of hop between STFT windows. (Default: ``win_length // 2``)
+#' @param n_fft (int, optional): Size of FFT, creates ``n_fft // 2 + 1`` bins. (Default: ``400``)
+#' @param f_min (float, optional): Minimum frequency. (Default: ``0.``)
+#' @param f_max (float or NULL, optional): Maximum frequency. (Default: ``NULL``)
+#' @param pad (int, optional): Two sided padding of signal. (Default: ``0``)
+#' @param n_mels (int, optional): Number of mel filterbanks. (Default: ``128``)
+#' @param window_fn (function, optional): A function to create a window tensor
+#'  that is applied/multiplied to each frame/window. (Default: ``torch_hann_window``)
+#' @param ... (optional): Arguments for window function.
+#'
+#' @return `tensor`: Mel frequency spectrogram of size (..., ``n_mels``, time).
+#'
+#' @section  Sources:
+#' - [https://gist.github.com/kastnerkyle/179d6e9a88202ab0a2fe]()
+#' - [https://timsainb.github.io/spectrograms-mfccs-and-inversion-in-python.html]()
+#' - [http://haythamfayek.com/2016/04/21/speech-processing-for-machine-learning.html]()
+#'
+#' @examples #'   Example
+#' sample_mp3 <- tuneR::readMP3(system.file("sample_audio_1.mp3", package = "torchaudio"))
+#' mel_specgram <- transform_mel_spectrogram(sample_rate = sample_mp3@@samp.rate)(sample_mp3@@left)  # (channel, n_mels, time)
+#'
+#' @export
+transform_mel_spectrogram <- torch::nn_module(
+  "MelSpectrogram",
+  initialize = function(
+    sample_rate = 16000,
+    n_fft = 400,
+    win_length = NULL,
+    hop_length = NULL,
+    f_min = 0.0,
+    f_max = NULL,
+    pad = 0,
+    n_mels = 128,
+    window_fn = torch::torch_hann_window,
+    power = 2.,
+    normalized = FALSE,
+    ...
+  ) {
+    self$sample_rate = sample_rate
+    self$n_fft = n_fft
+    self$win_length = if(!is.null(win_length)) win_length else n_fft
+    self$hop_length = if(!is.null(hop_length)) hop_length else self$win_length %/% 2
+    self$pad = pad
+    self$power = power
+    self$normalized = normalized
+    self$n_mels = n_mels  # number of mel frequency bins
+    self$f_max = f_max
+    self$f_min = f_min
+    self$spectrogram = transform_spectrogram(
+      n_fft = self$n_fft,
+      win_length = self$win_length,
+      hop_length = self$hop_length,
+      pad = self$pad,
+      window_fn = window_fn,
+      power = self$power,
+      normalized = self$normalized,
+      ...
+    )
+
+    self$mel_scale = transform_mel_scale(
+      n_mels = self$n_mels,
+      sample_rate = self$sample_rate,
+      f_min = self$f_min,
+      f_max = self$f_max,
+      n_stft = self$n_fft %/% 2 + 1
+    )
+  },
+
+  forward = function(waveform) {
+    specgram = self$spectrogram(waveform)
+    mel_specgram = self$mel_scale(specgram)
+    return(mel_specgram)
+  }
+)
+
 #' Mel-frequency Cepstrum Coefficients
 #'
 #' Create the Mel-frequency cepstrum coefficients from an audio signal.
@@ -239,7 +322,7 @@ transform_mfcc <- torch::nn_module(
     waveform = waveform$reshape(list(-1, shape[ls]))
 
     mel_specgram = self$mel_spectrogram(waveform)
-    if(selft$log_mels) {
+    if(self$log_mels) {
       log_offset = 1e-6
       mel_specgram = torch::torch_log(mel_specgram + log_offset)
     } else {
@@ -248,7 +331,8 @@ transform_mfcc <- torch::nn_module(
 
     # (channel, n_mels, time).tranpose(...) dot (n_mels, n_mfcc)
     # -> (channel, time, n_mfcc).tranpose(...)
-    mfcc = torch::torch_matmul(mel_specgram$transpose(2, 3), self$dct_mat)$transpose(2, 3)
+    browser()
+    mfcc = torch::torch_matmul(mel_specgram$transpose(3, 4), self$dct_mat)$transpose(3, 4)
 
     # unpack batch
     lspec = length(mfcc$shape)
