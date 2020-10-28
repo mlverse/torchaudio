bdfa5196cf489802a929da3982c91f390954f28e
Athos Damiani <athos.damiani@gmail.com>
2020-10-27 21:09:08 -0300

    tests for transform_mel_scale()

--- tests/testthat/test-transforms.R
+++ tests/testthat/test-transforms.R
@@ -1,3 +1,11 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+sample_mp3 <- tuneR::readMP3(system.file("sample_audio_1.mp3", package = "torchaudio"))
+
+test_that("transform_mel_scale", {
+  n_fft = 400
+  samples = length(sample_mp3@left)
+  expect_no_error(spec <- functional_spectrogram(sample_mp3@left, n_fft = n_fft))
+
+  ms = transform_mel_scale()(spec)
+  expect_tensor(ms)
+  expect_equal(dim(ms), c(1, 128, 1709))
 })
