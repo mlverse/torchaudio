8052fec676359b418711061eb011c0ebbff3c010
Athos Damiani <athos.damiani@gmail.com>
2020-11-02 16:47:02 -0300

    kaldi__get_lr_indices_and_weights() func tests docs

--- R/kaldi.R
+++ R/kaldi.R
@@ -1,3 +1,97 @@
+#' Linear Resample Indices And Weights
+#'
+#' Based on LinearResample::SetIndexesAndWeights where it retrieves the weights for
+#' resampling as well as the indices in which they are valid. LinearResample  (LR) means
+#' that the output signal is at linearly spaced intervals  (i.e the output signal has a
+#' frequency of ``new_freq``).
+#'
+#' @param orig_freq  (float): The original frequency of the signal
+#' @param new_freq  (float): The desired frequency
+#' @param output_samples_in_unit  (int): The number of output samples in the
+#'   smallest repeating unit: num_samp_out = new_freq / Gcd (orig_freq, new_freq)
+#' @param window_width  (float): The width of the window which is nonzero
+#' @param lowpass_cutoff  (float): The filter cutoff in Hz. The filter cutoff needs to be less
+#'            than samp_rate_in_hz/2 and less than samp_rate_out_hz/2.
+#' @param lowpass_filter_width  (int): Controls the sharpness of the filter, more == sharper but less
+#'            efficient. We suggest around 4 to 10 for normal use
+#'
+#' @return Tensor, Tensor): A tuple of ``min_input_index`` (which is the minimum indices
+#'         where the window is valid, size  (``output_samples_in_unit``)) and ``weights`` (which is the weights
+#'         which correspond with min_input_index, size  (``output_samples_in_unit``, ``max_weight_width``)).
+#'
+#' @details
+#'    It uses sinc/bandlimited interpolation to upsample/downsample
+#'    the signal.
+#'
+#'    The reason why the same filter is not used for multiple convolutions is because the
+#'    sinc function could sampled at different points in time. For example, suppose
+#'    a signal is sampled at the timestamps  (seconds)
+#'    0         16        32
+#'    and we want it to be sampled at the timestamps  (seconds)
+#'    0 5 10 15   20 25 30  35
+#'    at the timestamp of 16, the delta timestamps are
+#'    16 11 6 1   4  9  14  19
+#'    at the timestamp of 32, the delta timestamps are
+#'    32 27 22 17 12 8 2    3
+#'
+#'    As we can see from deltas, the sinc function is sampled at different points of time
+#'    assuming the center of the sinc function is at 0, 16, and 32  (the deltas [..., 6, 1, 4, ....]
+#'    for 16 vs [...., 2, 3, ....] for 32)
+#'
+#'    Example, one case is when the ``orig_freq`` and ``new_freq`` are multiples of each other then
+#'    there needs to be one filter.
+#'
+#'    A windowed filter function  (i.e. Hanning * sinc) because the ideal case of sinc function
+#'    has infinite support  (non-zero for all values) so instead it is truncated and multiplied by
+#'    a window function which gives it less-than-perfect rolloff [1].
+#'
+#'    [1] Chapter 16: Windowed-Sinc Filters, https://www.dspguide.com/ch16/1.htm
+#'
+#' @export
+kaldi__get_lr_indices_and_weights <- function(
+  orig_freq,
+  new_freq,
+  output_samples_in_unit,
+  window_width,
+  lowpass_cutoff,
+  lowpass_filter_width,
+  device,
+  dtype
+) {
+  if(lowpass_cutoff >= min(orig_freq, new_freq) / 2) value_error("lowpass_cutoff >= min(orig_freq, new_freq) / 2")
+  output_t = torch::torch_arange(0., output_samples_in_unit, device=device, dtype=dtype) / new_freq
+  min_t = output_t - window_width
+  max_t = output_t + window_width
+
+  min_input_index = torch::torch_ceil(min_t * orig_freq) # size (output_samples_in_unit)
+  max_input_index = torch::torch_floor(max_t * orig_freq) # size (output_samples_in_unit)
+  num_indices = max_input_index - min_input_index + 1 # size (output_samples_in_unit)
+
+  max_weight_width = num_indices$max()
+  # create a group of weights of size (output_samples_in_unit, max_weight_width)
+  j = torch::torch_arange(0, as.numeric(max_weight_width), device=device, dtype=dtype)$unsqueeze(1)
+  input_index = min_input_index$unsqueeze(2) + j
+  delta_t = (input_index / orig_freq) - output_t$unsqueeze(2)
+
+  weights = torch::torch_zeros_like(delta_t)
+  inside_window_indices = delta_t$abs()$lt(window_width)
+  # raised-cosine (Hanning) window with width `window_width`
+  weights[inside_window_indices] = 0.5 * (1 + torch::torch_cos(2 * pi * lowpass_cutoff /
+                                                                 lowpass_filter_width * delta_t[inside_window_indices]))
+
+  t_eq_zero_indices = delta_t$eq(0.0)
+  t_not_eq_zero_indices = !t_eq_zero_indices
+  # sinc filter function
+  weights[t_not_eq_zero_indices] = weights[t_not_eq_zero_indices] * torch::torch_sin(
+    2 * pi * lowpass_cutoff * delta_t[t_not_eq_zero_indices]) / (pi * delta_t[t_not_eq_zero_indices])
+  # limit of the function at t = 0
+  weights[t_eq_zero_indices] = weights[t_eq_zero_indices] * 2 * lowpass_cutoff
+
+  weights = weights / orig_freq # size (output_samples_in_unit, max_weight_width)
+  return(list(min_input_index, weights))
+}
+
+
 #' Kaldi's Resample Waveform
 #'
 #' Resamples the waveform at the new frequency.
--- tests/testthat/test-kaldi.R
+++ tests/testthat/test-kaldi.R
@@ -1,5 +1,17 @@
 test_that("kaldi__get_lr_indices_and_weights", {
-
+  expect_no_error(x <- kaldi__get_lr_indices_and_weights(
+  orig_freq = 4,
+  new_freq = 3,
+  output_samples_in_unit = 6,
+  window_width = 2,
+  lowpass_cutoff = 1,
+  lowpass_filter_width = 2,
+  device = torch::torch_device("cpu"),
+  dtype = torch::torch_float()
+  ))
+  expect_tensor(x[[1]])
+  expect_tensor(x[[2]])
+  expect_error(x[[3]])
 })
 
 test_that("kaldi_resample_waveform", {
