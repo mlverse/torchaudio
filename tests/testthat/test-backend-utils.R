aab52acf2c23b82c29b0349fbb10fc0caf5999f4
Athos Damiani <athos.damiani@gmail.com>
2020-11-02 00:31:19 -0300

    tests for %||%

--- tests/testthat/test-backend-utils.R
+++ tests/testthat/test-backend-utils.R
@@ -1,3 +1,8 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("%||%", {
+  expect_equal(NULL %||% 1, 1)
+  expect_equal(list() %||% 1, 1)
+  expect_equal(c() %||% 1, 1)
+  expect_equal(numeric(0) %||% 1, 1)
+  expect_equal(2 %||% 1, 2)
+  expect_equal(list(3) %||% 1, list(3))
 })
