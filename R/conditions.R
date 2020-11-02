34a776c54d98b033503c0eebcafc35393238b011
Athos Damiani <athos.damiani@gmail.com>
2020-11-02 18:02:22 -0300

    package_required() aux func

--- R/conditions.R
+++ R/conditions.R
@@ -25,3 +25,8 @@ is_torch_tensor <- function(x) {
 package_required_error <- function(pkg) {
   runtime_error(glue::glue("Package {pkg} required but not found. Please run install.packages('{pkg}') to install it."))
 }
+
+package_required <- function(pkg) {
+  if(!requireNamespace(pkg, quietly = TRUE))
+    package_required_error(pkg)
+}
