55f2193adbc91b333db489b955ada06051a2769f
Athos Damiani <athos.damiani@gmail.com>
2021-01-25 20:43:06 -0300

    torchaudio_load and torchaudio_loader generics

--- tests/testthat/test-backend-common.R
+++ tests/testthat/test-backend-common.R
@@ -12,23 +12,27 @@ test_that("tuneR_loader works", {
   a <- torchaudio:::tuneR_read_mp3_or_wav(filepath_mp3, from = 1, to = 40001, unit = "samples")
   # MP3
   by_samples <- tuneR_loader(filepath_mp3, offset = 1, duration = 40000, unit = "samples")
-
   expect_gte(length(by_samples@left), 40000)
   expect_lte(length(by_samples@left), 40000)
 
-  by_time <-tuneR_loader(filepath_mp3, offset = 1, duration = 1, unit = "time")
+  by_samples <- tuneR_loader(filepath_mp3, offset = 1, duration = 100, unit = "samples")
+  expect_gte(length(by_samples@left), 100)
+  expect_lte(length(by_samples@left), 100)
 
+  by_time <-tuneR_loader(filepath_mp3, offset = 1, duration = 1, unit = "time")
   expect_gt(length(by_time@left), 40000)
   expect_lt(length(by_time@left), 50000)
 
   # WAV
   by_samples <- tuneR_loader(filepath_wav, offset = 1, duration = 40000, unit = "samples")
-
   expect_gte(length(by_samples@left), 40000)
   expect_lte(length(by_samples@left), 40000)
 
-  by_time <-tuneR_loader(filepath_wav, offset = 1, duration = 1, unit = "seconds")
+  by_samples <- tuneR_loader(filepath_wav, offset = 1, duration = 100, unit = "samples")
+  expect_gte(length(by_samples@left), 100)
+  expect_lte(length(by_samples@left), 100)
 
+  by_time <-tuneR_loader(filepath_wav, offset = 1, duration = 1, unit = "seconds")
   expect_gte(length(by_time@left), 8000)
   expect_lte(length(by_time@left), 8000)
 })
@@ -43,4 +47,45 @@ test_that("transform_to_tensor works", {
 
   # numeric vector
   #...
+})
+
+test_that("set_audio_backend and torchaudio_loader works", {
+  loader <- getOption("torchaudio.loader")
+  expect_equal(class(loader), "function")
+
+  set_audio_backend(av_loader)
+  audio <- torchaudio_loader(filepath_mp3, offset = 1, duration = 100, unit = "samples")
+  expect_equal(class(audio)[1], "matrix")
+  expect_equal(length(audio), 100)
+
+  set_audio_backend(tuneR_loader)
+  audio <- torchaudio_loader(filepath_mp3, offset = 1, duration = 100, unit = "samples")
+  expect_equal(class(audio)[1], "Wave")
+  expect_equal(length(audio), 100)
+})
+
+test_that("torchaudio_load works", {
+  set_audio_backend(av_loader)
+  waveform_and_sample_rate <- torchaudio_load(filepath_mp3, offset = 1, duration = 100, unit = "samples")
+  expect_equal(class(waveform_and_sample_rate)[1], "list")
+  expect_equal(waveform_and_sample_rate[[2]], sample_mp3@samp.rate)
+  expect_tensor(waveform_and_sample_rate[[1]])
+
+  set_audio_backend(tuneR_loader)
+  waveform_and_sample_rate <- torchaudio_load(filepath_mp3, offset = 1, duration = 100, unit = "samples")
+  expect_equal(class(waveform_and_sample_rate)[1], "list")
+  expect_equal(waveform_and_sample_rate[[2]], sample_mp3@samp.rate)
+  expect_tensor(waveform_and_sample_rate[[1]])
+
+  set_audio_backend(av_loader)
+  waveform_and_sample_rate <- torchaudio_load(filepath_wav, offset = 1, duration = 100, unit = "samples")
+  expect_equal(class(waveform_and_sample_rate)[1], "list")
+  expect_equal(waveform_and_sample_rate[[2]], sample_wav@samp.rate)
+  expect_tensor(waveform_and_sample_rate[[1]])
+
+  set_audio_backend(tuneR_loader)
+  waveform_and_sample_rate <- torchaudio_load(filepath_wav, offset = 1, duration = 100, unit = "samples")
+  expect_equal(class(waveform_and_sample_rate)[1], "list")
+  expect_equal(waveform_and_sample_rate[[2]], sample_wav@samp.rate)
+  expect_tensor(waveform_and_sample_rate[[1]])
 })
