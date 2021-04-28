f2627cc08207a52c54b2ad8aaf88f9203f81c1a3
Daniel Falbel <dfalbel@gmail.com>
2021-04-28 13:37:11 -0300

    avoid check note

--- src/minimp3.h
+++ src/minimp3.h
@@ -4,7 +4,6 @@
     https://github.com/lieff/minimp3
     To the extent possible under law, the author(s) have dedicated all copyright and related and neighboring rights to this software to the public domain worldwide.
     This software is distributed without any warranty.
-    See <http://creativecommons.org/publicdomain/zero/1.0/>.
 */
 #include <stdint.h>
 
