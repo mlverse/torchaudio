33f6e9060a7ab9f04afb86f75d2fcd236fd1bd0c
Athos Damiani <athos.damiani@nubank.com.br>
2022-05-14 23:43:34 -0300

    workaround to avoid from and to args of tuneR::readMP3

--- R/backend-tuneR_backend.R
+++ R/backend-tuneR_backend.R
@@ -1,21 +1,28 @@
 #' @keywords internal
 tuneR_read_mp3_or_wav <- function(filepath, from = 0, to = Inf, unit = "samples") {
+  full_lenght = from == 0 & is.infinite(to)
+
   file_ext <- tools::file_ext(filepath)
   unit <- unit[1]
   if(file_ext == "mp3") {
-    info <- info(filepath)
-    to_ <- to
-    from_ <- from
-    duration <- info$num_frames
-    if(unit == "samples") {
-      from_ <-max(1, from_)/info$sample_rate
-      to_ <- to_/info$sample_rate
-      duration <- duration/info$sample_rate
+    if(full_lenght) {
+      wave_obj <- monitoR::readMP3(filepath)
+    } else {
+      info <- info(filepath)
+      to_ <- to
+      from_ <- from
+      duration <- info$num_frames
+      if(unit == "samples") {
+        from_ <-max(1, from_)/info$sample_rate
+        to_ <- to_/info$sample_rate
+        duration <- duration/info$sample_rate
+      }
+      to_ <- min(to_, duration)
+      to_ <- max(to_, from_ + 0.015)
+      to_ <- 0.05 + to_*1.01
+      wave_obj <- monitoR::readMP3(filepath, from = from_, to = to_)
     }
-    to_ <- min(to_, duration)
-    to_ <- max(to_, from_ + 0.015)
-    to_ <- 0.05 + to_*1.01
-    wave_obj <- monitoR::readMP3(filepath, from = from_, to = to_)
+
     if(from > 0 | is.finite(to))
       wave_obj <- tuneR::extractWave(wave_obj, from = unit=="samples", to = to - from, xunit = unit)
   } else if(file_ext == "wav") {
--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -5,6 +5,11 @@
 
 using namespace Rcpp;
 
+#ifdef RCPP_USE_GLOBAL_ROSTREAM
+Rcpp::Rostream<true>&  Rcpp::Rcout = Rcpp::Rcpp_cout_get();
+Rcpp::Rostream<false>& Rcpp::Rcerr = Rcpp::Rcpp_cerr_get();
+#endif
+
 // get_info_mp3
 Rcpp::List get_info_mp3(std::string filepath);
 RcppExport SEXP _torchaudio_get_info_mp3(SEXP filepathSEXP) {
