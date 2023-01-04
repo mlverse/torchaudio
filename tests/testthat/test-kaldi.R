93a22fa7d7ed648593bafd6a1f2b46627d565ad0
Sigrid Keydana <469371+skeydan@users.noreply.github.com>
2023-01-04 10:50:08 +0100

    address suggestions

--- tests/testthat/test-kaldi.R
+++ tests/testthat/test-kaldi.R
@@ -1,37 +1,33 @@
 test_that("kaldi__get_lr_indices_and_weights", {
-  if(requireNamespace("numbers", quietly = TRUE)) {
-    expect_no_error(x <- kaldi__get_lr_indices_and_weights(
-      orig_freq = 4,
-      new_freq = 3,
-      output_samples_in_unit = 6,
-      window_width = 2,
-      lowpass_cutoff = 1,
-      lowpass_filter_width = 2,
-      device = torch::torch_device("cpu"),
-      dtype = torch::torch_float()
-    ))
-    expect_tensor(x[[1]])
-    expect_tensor(x[[2]])
-    expect_error(x[[3]])
-  }
+  testthat::skip_if_not_installed("numbers")
+  expect_no_error(x <- kaldi__get_lr_indices_and_weights(
+    orig_freq = 4,
+    new_freq = 3,
+    output_samples_in_unit = 6,
+    window_width = 2,
+    lowpass_cutoff = 1,
+    lowpass_filter_width = 2,
+    device = torch::torch_device("cpu"),
+    dtype = torch::torch_float()
+  ))
+  expect_tensor(x[[1]])
+  expect_tensor(x[[2]])
+  expect_error(x[[3]])
 })
 
 test_that("kaldi__get_num_lr_output_samples", {
-  if(requireNamespace("numbers", quietly = TRUE)) {
-    expect_no_error(x <- kaldi__get_num_lr_output_samples(100, 10, 5))
-    expect_equal(x, 50)
-  }
+  testthat::skip_if_not_installed("numbers")
+  expect_no_error(x <- kaldi__get_num_lr_output_samples(100, 10, 5))
+  expect_equal(x, 50)
 })
 
 test_that("kaldi_resample_waveform", {
-  if(requireNamespace("numbers", quietly = TRUE)) {
-    expect_no_error(x <- kaldi_resample_waveform(
-      waveform = torch::torch_rand(2, 100),
-      orig_freq = 10,
-      new_freq = 9,
-      lowpass_filter_width = 6
-    ))
-    expect_tensor(x)
-  }
+  testthat::skip_if_not_installed("numbers")
+  expect_no_error(x <- kaldi_resample_waveform(
+    waveform = torch::torch_rand(2, 100),
+    orig_freq = 10,
+    new_freq = 9,
+    lowpass_filter_width = 6
+  ))
+  expect_tensor(x)
 })
-
