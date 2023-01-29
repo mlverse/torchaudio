22012d5dfee80b1e3d1cb361c8fdd55220f55327
Sigrid Keydana <469371+skeydan@users.noreply.github.com>
2023-01-29 11:53:01 +0100

    completely remove external library

--- R/torchaudio-package.R
+++ R/torchaudio-package.R
@@ -1,11 +1,4 @@
-## usethis namespace: start
-#' @importFrom Rcpp sourceCpp
-#' @useDynLib torchaudio, .registration = TRUE
-## usethis namespace: end
-NULL
-
 utils::globalVariables(c("..", "N"))
-
 
 .onLoad <- function(libname, pkgname) {
   op <- options(
