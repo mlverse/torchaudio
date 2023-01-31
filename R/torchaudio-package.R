50779589b1b62d13332809e16d89c711cea74663
Sigrid Keydana <469371+skeydan@users.noreply.github.com>
2023-01-31 10:55:23 +0100

    make av default

--- R/torchaudio-package.R
+++ R/torchaudio-package.R
@@ -2,7 +2,7 @@ utils::globalVariables(c("..", "N"))
 
 .onLoad <- function(libname, pkgname) {
   op <- options(
-    torchaudio.loader = tuneR_loader,
-    torchaudio.loader.name = "tuneR_loader"
+    torchaudio.loader = av_loader,
+    torchaudio.loader.name = "av_loader"
   )
 }
